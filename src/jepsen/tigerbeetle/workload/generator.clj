d9b5bc5d2e2e59d82cd881134437b5f36cbe2206
Kyle Kingsbury <aphyr@aphyr.com>
2024-11-13 17:09:46 -0600

    Start adding a proper generator

---
+++ src/jepsen/tigerbeetle/workload/generator.clj
@@ -0,0 +1,163 @@
+(ns jepsen.tigerbeetle.workload.generator
+  "Generates operations for the transfer workload. This is a giant mess of
+  monads, so we're breaking it out into its own namespace.
+
+  General goals:
+
+  - Generate *mostly* logically :ok events. Logical errors like
+  :exceeds-credits are fine and tell us something, but they don't change state,
+  and changing state is the name of the game.
+
+  - Heavy concurrency on some accounts. Not sure if we actually want low
+  concurrency or not.
+
+  - Reads of mostly recent accounts/transfers. Occasional reads of older
+  accounts.
+
+  - Continually adding accounts.
+
+  The name of the game, as usual, is exponential distributions. We're going to
+  create accounts continuously throughout the test and maintain an ordered list
+  of them. When selecting accounts for transfers/reads, we'll take an
+  exponential distribution over accounts. We'll order that distribution by
+  *hashmap* order, which means that some accounts get hot access, and others
+  are cooler, and newly created accounts can be either hot or cool."
+  (:require [bifurcan-clj [core :as b]
+                          [map :as bm]
+                          [list :as bl]
+                          [set :as bs]]
+            [clojure.core.match :refer [match]]
+            [clojure.data.generators :as dg]
+            [jepsen [generator :as gen]
+                    [util :as util]]
+            [potemkin :refer [definterface+]]))
+
+; The State encapsulates the information we need to know about the current
+; state of the database in order to generate new invocations. Mutations are
+; split into pairs: gen-*, which generates data for an invocation, and a
+; corresponding update function add-*, which folds that invocation into the
+; state when it is actually performed.
+(definterface+ IState
+  (gen-new-accounts [state n]
+               "Generates a series of n new accounts.")
+
+  (add-new-accounts [state accounts]
+                    "Called when we invoke create-accounts, to track that these
+                    accounts may now exist.")
+
+  (saw-accounts [state accounts]
+                 "Updates the state to acknowledge that we have positively
+                 observed a series of accounts."))
+
+(defrecord State
+  [
+   ^long next-id  ; The next ID we'll hand out
+   accounts       ; A Bifurcan map of id->account
+   transfers      ; A Bifurcan map of id->transfer
+   ; A Bifurcan set of account IDs we may have created, but haven't seen yet.
+   unseen-accounts
+   ; Ditto, transfers
+   unseen-transfers
+   ]
+
+  IState
+  (gen-new-accounts [this n]
+    (let [ids (range next-id (+ next-id n))]
+       (mapv (fn [id]
+               {:id        id
+                :ledger    (dg/long 1 3)
+                :code      (dg/long 1 10)
+                :user-data (dg/long 0 10)
+                :flags     #{}})
+             ids)))
+
+  (add-new-accounts [this accounts]
+    (let [ids (mapv :id accounts)]
+      (assoc this
+             :next-id (inc (reduce max next-id ids))
+             :unseen-accounts (b/forked
+                                (reduce bs/add
+                                       (b/linear unseen-accounts)
+                                       ids)))))
+
+  (saw-accounts [this accounts]
+    (assoc this
+           :unseen-accounts
+           (b/forked
+             (reduce bs/remove
+                     (b/linear unseen-accounts)
+                     (mapv :id accounts))))))
+
+(defn state
+  "A fresh state."
+  []
+  (map->State
+    {:next-id          1
+     :accounts         bm/empty
+     :transfers        bm/empty
+     :unseen-accounts  bs/empty
+     :unseen-transfers bs/empty}))
+
+; A generator which maintains the state and ensures its wrapped generator has
+; access to it via the context map.
+(defrecord GenContext [gen state]
+  gen/Generator
+  (op [this test ctx]
+      (when-let [[op gen'] (gen/op gen test (assoc ctx :state state))]
+        [op (GenContext. gen' state)]))
+
+  (update [this test ctx event]
+    (let [ctx (assoc ctx :state state)]
+      (match [(:type event) (:f event)]
+             ; When we create new accounts, advance our next account ID
+             [:invoke :create-accounts]
+             (GenContext. (gen/update gen test ctx event)
+                          (add-new-accounts state (:value event)))
+
+             [_ _]
+             this))))
+
+(defn create-accounts-gen
+  "A generator for create-accounts operations."
+  [test ctx]
+  {:f     :create-accounts
+   :value (gen-new-accounts (:state ctx) (rand-int 4))})
+
+(defn wrap-gen
+  "Wraps a generator in one that maintains our state."
+  [gen]
+  (map->GenContext
+    {:gen gen
+     :state (state)}))
+
+(defn gen
+  "Generator of events during the main phase"
+  []
+  (gen/mix
+    [create-accounts-gen]))
+
+(def final-gen-chunk-size
+  "Roughly how many things do we try to read per final read?"
+  128)
+
+(defn final-gen
+  "Final generator. Makes sure we try to observe every unseen account and
+  transfer."
+  []
+  (reify gen/Generator
+    (op [this test ctx]
+      ; Look up every unseen account.
+      (let [state           (:state ctx)
+            unseen-accounts (:unseen-accounts state)
+            n               (b/size unseen-accounts)
+            chunks          (b/split unseen-accounts
+                                     (/ n final-gen-chunk-size))]
+        (gen/op
+          (map (fn [ids]
+                 {:f :lookup-accounts, :value ids})
+               chunks)
+          test
+          ctx)))
+
+    (update [this test ctx event]
+      this)))
--- src/jepsen/tigerbeetle/workload/transfer.clj
+++ src/jepsen/tigerbeetle/workload/transfer.clj
@@ -89,60 +89,10 @@
                     [generator :as gen]
                     [util :refer [timeout]]]
             [jepsen.tigerbeetle.client :as c]
+            [jepsen.tigerbeetle.workload [generator :refer [final-gen
+                                                            gen
+                                                            wrap-gen]]]
             [potemkin :refer [definterface+]]))
-
-(defn gen-account
-  "Generates a single random account with the given ID"
-  [id]
-  {:id        id
-   :user-data (rand-int 10)
-   :ledger    (rand-int 10)
-   :code      (rand-int 10)
-   :flags     #{}})
-
-(defn create-accounts-gen
-  "Generates a new create-accounts operation"
-  [test context]
-  (let [id (:next-account-id context)]
-    {:type      :invoke
-     :f         :create-accounts
-     :value     (mapv gen-account (range id (+ id (rand-int 4))))}))
-
-(definterface+ IGenContext
-  (context [this ctx]
-           "Constructs a new context map passed on to our wrapped generator"))
-
-; Tracks generator context required to generate operations.
-(defrecord GenContext [gen ^long next-account-id]
-  IGenContext
-  (context [this ctx]
-    (assoc ctx :next-account-id next-account-id))
-
-  gen/Generator
-  (op [this test ctx]
-      (when-let [[op gen'] (gen/op gen test (context this ctx))]
-        [op (GenContext. gen' next-account-id)]))
-
-  (update [this test ctx event]
-    (let [ctx (context this ctx)]
-      (match [(:type event) (:f event)]
-
-             ; When we create new accounts, advance our next account ID
-             [:invoke :create-accounts]
-             (GenContext. (gen/update gen test ctx event)
-                          (inc (reduce max next-account-id
-                                       (mapv :id (:value event)))))
-
-             [_ _]
-             this))))
-
-(defn gen
-  "Generator of all events"
-  []
-  (map->GenContext
-    {:gen (gen/mix
-            [create-accounts-gen])
-     :next-account-id 0}))
 
 (defrecord Client [conn]
   client/Client
@@ -164,6 +114,8 @@
 (defn workload
   "Takes CLI opts and constructs a partial test map."
   [opts]
-  {:client    (Client. nil)
-   :generator (gen)
-   :checker   checker/unbridled-optimism})
+  {:client          (Client. nil)
+   :generator       (gen)
+   :final-generator (final-gen)
+   :wrap-generator  wrap-gen
+   :checker         checker/unbridled-optimism})
