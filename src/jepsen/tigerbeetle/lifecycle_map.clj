6d02851328e9602cba2ef42ea18c98e0e1ef153b
Kyle Kingsbury <aphyr@aphyr.com>
2024-12-12 12:50:22 -0600

    lifecycle-map: a data structure for generators
    Here, records go through a lifecycle of likely->unlikely->seen.

---
+++ src/jepsen/tigerbeetle/lifecycle_map.clj
@@ -0,0 +1,173 @@
+(ns jepsen.tigerbeetle.lifecycle-map
+  "Transfers and accounts go through several phases during the course of a
+  test. When we attempt to create something, we call it *possible*. When not
+  yet observed, we call it *unseen*. Once observed, we call it *seen*. There is
+  a very good chance that many of our possible, unseen records do not actually
+  exist--for instance, if the op returns :fail, or the result of that
+  individual record is an error like :linked-event-failed, or we try to read it
+  and do not see it. We call these records *unlikely*, and other unseen records
+  *likely*. When trying to read and update objects, we want to focus our
+  efforts mainly on the seen and likely ones, not the unlikely ones."
+  (:require [bifurcan-clj [core :as b]
+                          [list :as bl]
+                          [map :as bm]
+                          [set :as bs]]
+            [clojure.data.generators :as dg]
+            [dom-top.core :refer [loopr]]
+            [potemkin :refer [definterface+]])
+  (:import (io.lacuna.bifurcan ISet
+                               Lists
+                               Sets
+                               Maps)
+           (java.util OptionalLong)
+           (java.util.function Function
+                               LongFunction)))
+
+;; Lazy, virtual data structures formed by gluing together n disjoint
+;; structures.
+
+(defn lazy-list-concat
+  "Lazy list concat for Bifurcan."
+  [lists]
+  (case (count lists)
+    0 bl/empty
+    1 (first lists)
+    (let [size (reduce + (map b/size lists))
+          nth  (reify LongFunction
+                 (apply [_ i]
+                   (loopr [i i]
+                          [list lists]
+                          (let [size (b/size list)]
+                            (if (< i size)
+                              ; It's in this list
+                              (b/nth list i)
+                              ; Try the next one
+                              (recur (- i size)))))))]
+          (Lists/from size nth))))
+
+(defn lazy-set-union
+  "Lazy set union for Bifurcan. ONLY works if the sets are disjoint--for our
+  purposes, they always are."
+  [sets]
+  (case (count sets)
+    0 bs/empty
+    1 (first sets)
+    (let [; List of elements
+          size (reduce + (map b/size sets))
+          nth  (reify LongFunction
+                 (apply [_ i]
+                   (loopr [i i]
+                          [set sets]
+                          (let [size (b/size set)]
+                            (if (< i size)
+                              ; It's in this set
+                              (b/nth set i)
+                              ; Try the next one
+                              (recur (- i size)))))))
+          elements (Lists/from size nth)
+          ; Function for the index of an element
+          index-of (reify Function
+                     (apply [_ x]
+                       (loopr [i 0] ; Index of first element in set
+                              [set sets]
+                              (if-let [j (bs/index-of set x)]
+                                (OptionalLong/of (+ i j))
+                                (recur (+ i (b/size set))))
+                              (OptionalLong/empty))))]
+      (Sets/from elements, index-of))))
+
+(defn lazy-map-union
+  "Lazy map union for Bifurcan. ONLY works if the maps are disjoint--for our
+  purposes, they always are."
+  [maps]
+  (let [ks (lazy-set-union (mapv bm/keys maps))
+        to-v (reify Function
+               (apply [_ k]
+                 (loopr []
+                        [m maps]
+                        (let [v (bm/get m k ::not-found)]
+                          (if (identical? ::not-found v)
+                            (recur)
+                            v))
+                        (throw (IllegalStateException.
+                                 (str "Expected a value for key " k))))))]
+    (Maps/from ks to-v)))
+
+(definterface+ ILifecycleMap
+  (is-possible [m id x]
+                 "Adds a possible record to the map.")
+
+  (is-seen [m id]
+           "Indicates that we positively observed the given id.")
+
+  (is-unseen [m p id]
+             "Indicates that we failed to observe the given ID. If seen, no
+             effect. If unseen, With probability p, moves id from likely to
+             unlikely.")
+
+  (possible [m]
+            "A map of ids to possible records.")
+
+  (seen [m]
+        "A map of ids to seen records.")
+
+  (unseen [m]
+          "A map of ids to unseen records.")
+
+  (likely [m]
+            "A map of ids to likely records.")
+
+  (unlikely [m]
+            "A map of ids to unlikely records."))
+
+
+; We divide records into disjoint maps of id->record:
+; seen      - Definitely seen
+; likely    - Unseen and likely
+; unlikely  - Unseen and unlikely
+(defrecord LifecycleMap [seen likely unlikely]
+  ILifecycleMap
+  (is-possible [m id x]
+    (LifecycleMap. seen (bm/put likely id x) unlikely))
+
+  (is-seen [this id]
+    (if-let [x (or (bm/get likely id)
+                   (bm/get unlikely id))]
+      (LifecycleMap. (bm/put seen id x)
+                     (bm/remove likely id)
+                     (bm/remove unlikely id))
+      (throw (IllegalStateException.
+               (str "Can't see id that was never added:" (pr-str id))))))
+
+  (is-unseen [this p id]
+    (if (or (bm/contains? seen id)
+            (< p (dg/double)))
+      ; No change
+      this
+      ; Swap to unlikely
+      (if-let [x (bm/get likely id)]
+        (LifecycleMap. seen
+                       (bm/remove likely id)
+                       (bm/put unlikely id x))
+        ; Already unlikely
+        this)))
+
+  (possible [this]
+    (lazy-map-union [seen likely unlikely]))
+
+  (seen [this]
+    seen)
+
+  (unseen [this]
+    (lazy-map-union [likely unlikely]))
+
+  (likely [this]
+    likely)
+
+  (unlikely [this]
+    unlikely))
+
+(defn lifecycle-map
+  "Constructs an empty lifecycle map."
+  []
+  (LifecycleMap. bm/empty bm/empty bm/empty))
---
+++ test/jepsen/tigerbeetle/lifecycle_map_test.clj
@@ -0,0 +1,144 @@
+(ns jepsen.tigerbeetle.lifecycle-map-test
+  (:require [bifurcan-clj [core :as b]
+                          [list :as bl]
+                          [map :as bm]
+                          [set :as bs]]
+            [clojure [datafy :refer [datafy]]
+                     [pprint :refer [pprint]]
+                     [test :refer :all]]
+            [clojure.test.check [clojure-test :refer [defspec]]
+                                [generators :as gen]
+                                [properties :as prop]
+                                [results :as results :refer [Result]]
+                                [rose-tree :as rose]]
+            [com.gfredericks.test.chuck.clojure-test :as chuck :refer [checking for-all]]
+            [dom-top.core :refer [loopr]]
+            [jepsen.tigerbeetle [lifecycle-map :refer :all]]))
+
+(def test-n 100)
+
+(def list-gen
+  "Generator of Lists."
+  (gen/let [xs (gen/vector gen/nat)]
+    (bl/from xs)))
+
+(def set-gen
+  "Generator of Sets."
+  (gen/let [xs (gen/set gen/nat)]
+    (bs/from xs)))
+
+(def disjoint-sets-gen
+  "Generates a vector of disjoint sets."
+  (gen/let [sets (gen/vector set-gen)]
+    (loopr [used (b/linear bs/empty)
+            sets' []]
+           [set sets]
+           (recur (bs/union used set)
+                  (conj sets' (bs/difference set used)))
+           sets')))
+
+(defn cut
+  "Cuts a vector xs into subvectors from [0,i0), [i0,i1) ... [in-1, size(xs))"
+  [xs is]
+  (assert (vector? is))
+  (assert (vector? xs))
+  (->> (conj is (count xs))
+       (cons 0)
+       (partition 2 1)
+       (mapv (fn [[i i']]
+               (subvec xs i i')))))
+
+(def disjoint-maps-gen
+  "Generates a vector of maps with disjoint keys."
+  (gen/let [n    gen/nat
+            ks   (gen/vector-distinct gen/nat {:num-elements n
+                                               :max-tries 100})
+            vs   (gen/vector gen/nat n)
+            cuts (if (pos? n)
+                   (gen/vector-distinct
+                     (gen/large-integer* {:min 0 :max (dec n)})
+                     {:max-elements (long (/ n 3))})
+                   (gen/return []))]
+    (let [cuts (vec (sort cuts))]
+      (mapv (comp bm/from zipmap)
+            (cut ks cuts)
+            (cut vs cuts)))))
+
+(deftest lazy-list-concat-test
+  (checking "concat" test-n
+            [lists (gen/vector list-gen)]
+            (is (= (bl/concat-all lists)
+                   (lazy-list-concat lists)))))
+
+(defn set-union
+  "Reducer form of bifurcan set union"
+  ([] bs/empty)
+  ([x] x)
+  ([x y] (bs/union x y)))
+
+(deftest lazy-set-union-test
+  (checking "union" test-n
+            [sets disjoint-sets-gen]
+            (is (= (reduce set-union sets)
+                   (lazy-set-union sets)))))
+
+(defn map-union
+  "Reducer form of bifurcan map union"
+  ([] bm/empty)
+  ([x] x)
+  ([x y] (bm/union x y)))
+
+(deftest lazy-map-union-test
+  (checking "union" test-n
+            [maps disjoint-maps-gen]
+            (is (= (reduce map-union maps)
+                   (lazy-map-union maps)))))
+
+(defn d=
+  "Equals, with datafy on the second arg."
+  [expected actual]
+  (= expected (datafy actual)))
+
+(deftest lifecycle-map-test
+  (testing "empty"
+    (let [m (lifecycle-map)]
+      (is (d= {} (possible m)))
+      (is (d= {} (seen m)))
+      (is (d= {} (unseen m)))
+      (is (d= {} (likely m)))
+      (is (d= {} (unlikely m)))))
+
+  (testing "possible"
+    (let [m (-> (lifecycle-map)
+                (is-possible :x 0))]
+      (is (d= {:x 0} (possible m)))
+      (is (d= {} (seen m)))
+      (is (d= {:x 0} (unseen m)))
+      (is (d= {:x 0} (likely m)))
+      (is (d= {} (unlikely m)))))
+
+  (testing "unseen"
+    (let [m (-> (lifecycle-map)
+                (is-possible :x 0)
+                (is-possible :y 1)
+                (is-unseen 0 :x)  ; 0 probability
+                (is-unseen 1 :y))] ; 1 probability
+      (is (d= {:x 0, :y 1} (possible m)))
+      (is (d= {} (seen m)))
+      (is (d= {:x 0, :y 1} (unseen m)))
+      (is (d= {:x 0} (likely m)))
+      (is (d= {:y 1} (unlikely m)))))
+
+  (testing "seen"
+    (let [m (-> (lifecycle-map)
+                (is-possible :x 0)
+                (is-possible :y 1)
+                (is-seen :y)
+                (is-unseen 0 :x)
+                (is-unseen 1 :y)
+                (is-seen :x))]
+      (is (d= {:x 0, :y 1} (possible m)))
+      (is (d= {:x 0, :y 1} (seen m)))
+      (is (d= {} (unseen m)))
+      (is (d= {} (likely m)))
+      (is (d= {} (unlikely m))))))
