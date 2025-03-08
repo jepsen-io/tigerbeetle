b92ebda20ec3b091ec2864729d81864f04de5411
Kyle Kingsbury <aphyr@aphyr.com>
2025-03-07 18:08:17 -0600

    Version 0.1.1-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject jepsen.tigerbeetle "0.1.0"
+(defproject jepsen.tigerbeetle "0.1.1-SNAPSHOT"
   :description "Jepsen tests for the TigerBeetle financial database"
   :url "https://github.com/jepsen-io/tigerbeetle"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
